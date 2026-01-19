b8b44c929f65caa9a1c8255b40d058074a8d1185
sagar-johari <joharisagar53@gmail.com>
2026-01-19 16:18:17 +0530

    changed the prisma client object

--- src/controllers/dm.controller.js
+++ src/controllers/dm.controller.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../config/prisma.js";
 
 /**
  * Create or get existing DM between two users
--- src/controllers/thread.controller.js
+++ src/controllers/thread.controller.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../config/prisma.js";
 
 /**
  * Get all replies (messages) for a parent message
