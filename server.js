9929711dff373f425763e03d2e171023c4c333c9
sagar-johari <joharisagar53@gmail.com>
2026-03-05 12:16:18 +0530

    changes

--- server.js
+++ server.js
@@ -35,12 +35,14 @@ const allowedOrigins = [
   "http://192.168.1.14:3000",
   "http://192.168.1.15:3000",
   "http://192.168.0.113:5000",
+  "http://192.168.0.113:5000",
+  "http://192.168.31.147:3000",
   "https://test-socket-client-steel.vercel.app/",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
   })
 );
